623a1e08d77c24fd5ac952ba348372536074b073
upadrastaharshavardhan <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-05-12 13:41:26 +0530

    Create banner.py

---
+++ common/banner.py
@@ -0,0 +1,32 @@
+import sys
+from common.colors import bannerblue, bannerblue2, W, Y, R, end
+
+
+def banner():
+    print("""%s
+░▄▄▄       ██░ ██   ██████  ██▀███   ▄▄▄       ██░ ██ 
+▒████▄    ▓██░ ██▒▒██    ▒ ▓██ ▒ ██▒▒████▄    ▓██░ ██▒
+▒██  ▀█▄  ▒██▀▀██░░ ▓██▄   ▓██ ░▄█ ▒▒██  ▀█▄  ▒██▀▀██░
+░██▄▄▄▄██ ░▓█ ░██   ▒   ██▒▒██▀▀█▄  ░██▄▄▄▄██ ░▓█ ░██ 
+ ▓█   ▓██▒░▓█▒░██▓▒██████▒▒░██▓ ▒██▒ ▓█   ▓██▒░▓█▒░██▓
+ ▒▒   ▓▒█░ ▒ ░░▒░▒▒ ▒▓▒ ▒ ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░▒░▒
+  ▒   ▒▒ ░ ▒ ░▒░ ░░ ░▒  ░ ░  ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░▒░ ░
+  ░   ▒    ░  ░░ ░░  ░  ░    ░░   ░   ░   ▒    ░  ░░ ░
+      ░  ░ ░  ░  ░      ░     ░           ░  ░ ░  ░  ░
+                                                      
+    %s# Coded By harsha69 -%s @pasyanthharsha      
+                                           %s"""
+          %
+          (bannerblue, bannerblue2,
+           W, bannerblue2, W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2,
+           W, bannerblue2,
+           W, Y, end
+           ))
